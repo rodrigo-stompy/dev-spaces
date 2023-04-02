8832fe4b5692b7d73ab91382992a47cab66495b1
Rodrigo Garcia <rodrigo@pachanga.local>
2023-04-01 17:23:01 -0700

    Implement calling ChatGPT.

--- src/main/kotlin/app/stompy/chatgptjavadoc/chatgpt_javadoc/ChatgptJavadocAction.kt
+++ src/main/kotlin/app/stompy/chatgptjavadoc/chatgpt_javadoc/ChatgptJavadocAction.kt
@@ -4,12 +4,31 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.PsiMethod
+import com.theokanning.openai.completion.CompletionRequest
+import com.theokanning.openai.service.OpenAiService
 import org.jetbrains.annotations.NotNull
 
 class ChatgptJavadocAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent) {
-        TODO("Not yet implemented")
+    override fun actionPerformed(event: AnActionEvent) {
+        val element = event.getData(CommonDataKeys.PSI_ELEMENT)
+        if (element !is PsiMethod) {
+            throw IllegalStateException("Event element is not a method!")
+        }
+
+        val start = element.getTextRange().startOffset
+        val methodText = element.text
+        val javadoc = generateJavadoc(methodText)
+
+        val editor = event.getRequiredData(CommonDataKeys.EDITOR)
+        val project = event.getRequiredData(CommonDataKeys.PROJECT)
+        val document = editor.document
+
+        // Must do this document change in a write action context.
+        WriteCommandAction.runWriteCommandAction(
+            project
+        ) { document.insertString(start, "/**\n $javadoc*/\n") }
     }
 
     override fun update(event: @NotNull AnActionEvent) {
@@ -19,5 +38,29 @@ class ChatgptJavadocAction : AnAction() {
 
     override fun getActionUpdateThread(): ActionUpdateThread {
         return ActionUpdateThread.BGT
+    }
+
+    private fun generateJavadoc(methodText: String): String? {
+        // TODO: Read key from the environment. Please don't abuse my free quota!
+        val service = OpenAiService("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")
+        val completionRequest: CompletionRequest = CompletionRequest.builder()
+            .model("text-davinci-003")
+            .prompt(
+    """
+    $methodText
+    // An elaborate, high quality JavaDoc for the above function:
+    /**
+    
+    """.trimIndent()
+            )
+            .echo(false)
+            .user("testing")
+            .n(1)
+            .maxTokens(150)
+            .stop(listOf("//", "*/"))
+            .temperature(0.0)
+            .build()
+        // TODO: Handle no choices returned.
+        return service.createCompletion(completionRequest).choices[0].text
     }
 }
