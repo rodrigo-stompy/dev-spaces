4d9e40b1783ebd11f91fe0f2180e52c92279c6a3
Rodrigo Garcia <rodrigo@pachanga.local>
2023-04-03 14:09:53 -0700

    Improve style

--- src/main/kotlin/app/stompy/chatgptjavadoc/chatgpt_javadoc/ChatgptJavadocAction.kt
+++ src/main/kotlin/app/stompy/chatgptjavadoc/chatgpt_javadoc/ChatgptJavadocAction.kt
@@ -8,7 +8,6 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.PsiMethod
 import com.theokanning.openai.completion.CompletionRequest
 import com.theokanning.openai.service.OpenAiService
-import org.jetbrains.annotations.NotNull
 
 class ChatgptJavadocAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
@@ -17,27 +16,23 @@ class ChatgptJavadocAction : AnAction() {
             throw IllegalStateException("Event element is not a method!")
         }
 
-        val start = element.getTextRange().startOffset
-        val methodText = element.text
-
         // TODO: This is a blocking call. Consider how to make this async.
-        val javadoc = generateJavadocWithChatGpt(methodText)
+        val javadoc = generateJavadocWithChatGpt(element.text)
 
         val editor = event.getRequiredData(CommonDataKeys.EDITOR)
         val project = event.getRequiredData(CommonDataKeys.PROJECT)
         val document = editor.document
 
         // Must do this document change in a write action context.
-        WriteCommandAction.runWriteCommandAction(
-            project
-        ) {
-            // TODO: The indentation in the code is not right. Consider
-            //  triggering a reformat.
-            document.insertString(start, "/**\n $javadoc*/\n")
+        WriteCommandAction.runWriteCommandAction(project) {
+            val start = element.getTextRange().startOffset
+            // TODO: The indentation level is not always right depending on
+            //  nesting. Consider triggering a reformat.
+            document.insertString(start, javadoc)
         }
     }
 
-    override fun update(event: @NotNull AnActionEvent) {
+    override fun update(event: AnActionEvent) {
         val element = event.getData(CommonDataKeys.PSI_ELEMENT)
         event.presentation.isEnabledAndVisible = element is PsiMethod
     }
@@ -47,21 +42,14 @@ class ChatgptJavadocAction : AnAction() {
     }
 
     /** Uses ChatGPT to generate a JavaDoc for the selected method. */
-    private fun generateJavadocWithChatGpt(methodText: String): String? {
+    private fun generateJavadocWithChatGpt(methodText: String): String {
         // TODO: Read key from the environment. Please don't abuse my free quota!
         val service = OpenAiService("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")
         val completionRequest: CompletionRequest = CompletionRequest.builder()
             .model("text-davinci-003")
             // TODO: Consider adding multiple actions for other prompts, e.g.:
             //  "A concise, high quality JavaDoc"
-            .prompt(
-    """
-    $methodText
-    // An elaborate, high quality JavaDoc for the above function:
-    /**
-    
-    """.trimIndent()
-            )
+            .prompt(buildPromptForElaborateJavaDoc(methodText))
             .echo(false)
             .user("testing")
             .n(1)
@@ -71,7 +59,26 @@ class ChatgptJavadocAction : AnAction() {
             .temperature(0.0)
             .build()
 
-        // TODO: Handle no choices returned.
-        return service.createCompletion(completionRequest).choices[0].text
+        // TODO: Handle no choices returned more gracefully.
+        val completion = service.createCompletion(completionRequest).choices[0].text
+        return formatJavaDocCompletion(completion)
+    }
+
+    private fun buildPromptForElaborateJavaDoc(methodText: String): String {
+        // TODO: Remove assumption that method is indented by two spaces.
+        // The trailing line-break and spaces are there to make the formatting
+        // in the completion more predictable.
+        return buildString {
+            append("  ")
+            append(methodText)
+            append("\n  // An elaborate, high quality JavaDoc for the above function:")
+            append("\n  /**")
+            append("\n  ")
+        }
+    }
+
+    private fun formatJavaDocCompletion(completion: String?): String {
+        // TODO: Remove assumption that method is indented by two spaces.
+        return if (completion != null) "/**\n  $completion*/\n  " else ""
     }
 }
